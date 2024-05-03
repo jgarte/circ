086b0ba83bc1ff2ffecbf168ee4f5c063a9c1392
Leonora <147441833+leonorae@users.noreply.github.com>
2024-05-03 02:05:55 -0500

    Add files via upload

--- circular-buffer.lisp
+++ circular-buffer.lisp
@@ -2,43 +2,49 @@
   buffer
   size
   (read 0)
-  (write 0))
+  (write 0)
+  (length 0))
 
 (defun new-circ (size)
   (make-circ :buffer (make-array size
                                  :initial-element nil)
-             :size (1+ size)
+             :size size
              :read 0
-             :write 0))
+             :write 0
+             :length 0))
 
 (defun write-to (circ num)
   (let* ((cur-write (circ-write circ))
          (next-write (mod (1+ cur-write)
                           (circ-size circ))))
-    (unless (= next-write (circ-read circ))
+    (unless (>= (circ-length circ)
+                (circ-size circ))
       (setf (aref (circ-buffer circ)
                   cur-write)
             num)
       (setf (circ-write circ)
-            next-write))))
+            next-write)
+      (incf (circ-length circ)))))
 
 (defun read-from (circ)
   (let ((cur-read (circ-read circ)))
-    (unless (= cur-read (circ-write circ))
+    (unless (= (circ-length circ) 0)
       (setf (circ-read circ)
             (mod (1+ cur-read)
                  (circ-size circ)))
+      (decf (circ-length circ))
       (aref (circ-buffer circ) cur-read))))
 ;; demonstration
 (defun fill-buffer (circ)
-  (let ((x (circ-write circ)))
+  (let ((x 1))
     (loop while (write-to circ x)
           do (setf x (1+ x)))))
 
 (defun read-buffer (circ)
   (let ((result (read-from circ)))
     (loop while result
-          do (print result))))
+          do (print result)
+          (setf result (read-from circ)))))
 
 
 
